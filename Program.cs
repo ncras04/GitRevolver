03185606ba79410302aff2667ed44fcb0e6e0905
Nicolas Schmitt <nschmitt@duck.com>
2026-04-06 14:15:31 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TSCHÜSSKE!");
+            Console.WriteLine("ROFLCOPTER!");
+        }
+    }
+}
